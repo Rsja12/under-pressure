31fa707ae576985c7a6a745eca8ab8531e4458c1
Rsja12 <asocarras10@gmail.com>
2019-12-03 18:24:07 -0500

    updates log form

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -15,15 +15,16 @@ class LogsController < ApplicationController
         if @log.save
             redirect_to logs_path
         else
-         
             render :new 
         end
     end
 
     private
 
+
+
     def log_params
-        params.require(:log).permit(:date, :remarks, :dive_site_name, :dive_buddy, :depth, :dive_time, :visibility, :dive_number, :user_id, :dive_site_id)
+        params.require(:log).permit(:date, :remarks, :dive_site_name, :dive_buddy, :depth, :dive_time, :visibility, :user_id, :dive_site_id)
     end
 
 end
