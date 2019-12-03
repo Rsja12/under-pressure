7da483c3eec3fb950c71dd543185394d17f1ed13
Rsja12 <asocarras10@gmail.com>
2019-12-03 13:46:45 -0500

    Removes Review resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :logs
-  resources :reviews
   resources :dive_sites
   devise_for :users, :controllers => {registrations: 'registrations' }
   root to: 'application#welcome'
