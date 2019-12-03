5a38aa0bc75b2981ba54b3324672c01a67214917
Rsja12 <asocarras10@gmail.com>
2019-12-03 17:33:19 -0500

    Creates log index action and index view that lists out each log by dive site name

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,8 +1,20 @@
 class LogsController < ApplicationController
 
+    def index
+        @logs = Log.all 
+    end
+
     def new
         @log = Log.new
     end
 
-   
+    # def create
+        
+    # end
+
+    # private
+
+    # def log_params
+    #     require(:log).permit()
+    # end
 end
