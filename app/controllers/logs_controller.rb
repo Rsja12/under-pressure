d8268d7d055f29643347a2cb34fca58ace541b01
Rsja12 <asocarras10@gmail.com>
2019-12-04 11:33:38 -0500

    removes actions from DiveSitesController. Sets log's user id = current_user.id. fixesfields_for in log form

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -5,16 +5,17 @@ class LogsController < ApplicationController
     end
 
     def show
-        @log = Log.find_by(id: params[:id])
+        @log = Log.find(params[:id])
     end
 
     def new
         @log = Log.new
-        @log.build_dive_site #What does this do? 
+        @log.build_dive_site 
     end
 
     def create
         @log = Log.new(log_params)
+        @log.user_id = current_user.id 
         if @log.save
             redirect_to log_path(@log)
         else
@@ -26,7 +27,7 @@ class LogsController < ApplicationController
     private
 
     def log_params
-        params.require(:log).permit(:date, :remarks, :dive_buddy, :depth, :dive_time, :visibility, :user_id, :dive_site_id, dive_site_attributes: [:name])
+        params.require(:log).permit(:date, :remarks, :dive_buddy, :depth, :dive_time, :visibility, :user_id, :dive_site_id, dive_site_attributes: [:name] )
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :logs
   resources :dive_sites
   devise_for :users, :controllers => {registrations: 'registrations' }
+  
   root to: 'application#welcome'
 end
