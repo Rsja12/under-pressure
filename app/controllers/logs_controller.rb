f4d7c57e27b2a06704e869b5b7fe7c8568a583bf
Rsja12 <asocarras10@gmail.com>
2019-12-05 17:57:50 -0500

    Updates new and create actiions in logs_controller. Adds min number for form number_fields.

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,30 +3,36 @@ class LogsController < ApplicationController
     def index
         if @dive_site = DiveSite.find_by(id: params[:dive_site_id])
             @logs = @dive_site.logs
-            # byebug
         else
             @logs = Log.all 
         end
     end
 
-    def show
-        @log = Log.find(params[:id])
-    end
-
     def new
-        @log = Log.new
-        @log.build_dive_site 
+        if @dive_site = DiveSite.find_by(id: params[:dive_site_id])
+            @logs = @dive_site.logs.build
+        else
+            @log = Log.new
+            # @log.build_dive_site 
+        end
     end
-
+    
     def create
-        @log = Log.new(log_params)
-        @log.user_id = current_user.id 
+        # byebug
+        @log = current_user.logs.build(log_params)
+        # @log = Log.new(log_params)
+        # @log.user_id = current_user.id 
+        # @log.save 
         if @log.save
             redirect_to log_path(@log)
         else
             @log.build_dive_site
             render :new 
         end
+    end
+    
+    def show
+        @log = Log.find_by(id: params[:id])
     end
 
     private
