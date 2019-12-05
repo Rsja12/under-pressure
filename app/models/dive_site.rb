fbae1ff2cba2e8228d4b6da92753c29557d56e5d
Rsja12 <asocarras10@gmail.com>
2019-12-05 12:17:47 -0500

    Adds dependent destroy for logs DiveSite model

--- app/models/dive_site.rb
+++ app/models/dive_site.rb
@@ -1,6 +1,7 @@
 class DiveSite < ApplicationRecord
-    has_many :logs 
+    has_many :logs, dependent: :destroy
     has_many :users, through: :logs 
+
 
     validates :name, presence: true
     validates :name, uniqueness: true
