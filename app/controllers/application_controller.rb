40a33fa49e7956a2b1a8fad7c11bf30c7fa1395f
Rsja12 <asocarras10@gmail.com>
2019-12-08 10:37:59 -0500

    updates application#dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
     end
 
     def dashboard
+        @user = current_user
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   
-    root :to => 'application#welcome'
-    get 'dashboard', to: 'application#dashboard'
+  root :to => 'application#welcome'
+
+  get 'dashboard', to: 'application#dashboard'
 
   resources :logs
-
+  
   resources :dive_sites do
     resources :logs, only: [:index, :new]
   end
